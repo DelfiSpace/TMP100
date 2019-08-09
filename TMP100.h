5b242a717bc23f70d3266240f01cb41f19c9386e
Stefano Speretta <s.speretta@tudelft.nl>
2019-08-09 14:00:43 +0200

    Reverting to previous commit

--- TMP100.h
+++ TMP100.h
@@ -1,52 +1,86 @@
 /* Code written by Chia Jiun Wei @ 1 Mar 2017
  * <J.W.Chia@tudelft.nl>
-
- * TMP100: a library to provide high level APIs to interface with the
- * Texas Instruments temperature sensor. It is possible to use this
- * library in Energia (the Arduino port for MSP microcontrollers) or
+ 
+ * TMP100: a library to provide high level APIs to interface with the 
+ * Texas Instruments temperature sensor. It is possible to use this 
+ * library in Energia (the Arduino port for MSP microcontrollers) or 
  * in other toolchains.
-
+ 
  * This file is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License
+ * it under the terms of the GNU Lesser General Public License 
  * version 3, both as published by the Free Software Foundation.
-
+  
  */
+ 
+#ifndef TMP100_H
+#define TMP100_H
 
-#ifndef __TMP100_H
-#define __TMP100_H
+#include <Energia.h>
+#include <DWire.h>
 
-#include "satellite.h"
-#include <stdint.h>
+//Internal Register Address
+#define TEMP_REG					0x00
+#define CONFIG_REG					0x01
+#define TEMP_THRESH_HIGH_REG		0x02
+#define TEMP_THRESH_LOW_REG			0x03
 
-//Config Register Settings
+//Config Register Settings 
 //Shutdown Mode
-#define TMP100_ENABLE_SHUTDOWN				0x01
-#define TMP100_DISABLE_SHUTDOWN			0x00
+#define ENABLE_SHUTDOWN				0x01
+#define DISABLE_SHUTDOWN			0x00
 
 //Thermostat mode
-#define TMP100_COMPARATOR_MODE				0x00
-#define TMP100_INTERRUPT_MODE				0x02
+#define COMPARATOR_MODE				0x00
+#define INTERRUPT_MODE				0x02
 
-//Polarity
-#define TMP100_POLARITY_LOW				0x00		//Active Low
-#define TMP100_POLARITY_HIGH				0x04		//Active High
+//Polarity 
+#define POLARITY_LOW				0x00		//Active Low
+#define POLARITY_HIGH				0x04		//Active High
 
 //Fault queue
-#define TMP100_CONSECUTIVE_FAULTS_1		0x00
-#define TMP100_CONSECUTIVE_FAULTS_2		0x08
-#define TMP100_CONSECUTIVE_FAULTS_4		0x10
-#define TMP100_CONSECUTIVE_FAULTS_6		0x18
+#define CONSECUTIVE_FAULTS_1		0x00
+#define CONSECUTIVE_FAULTS_2		0x08
+#define CONSECUTIVE_FAULTS_4		0x10	
+#define CONSECUTIVE_FAULTS_6		0x18	
 
 //Converter Resolution
-#define TMP100_RESOLUTION_9_BIT			0x00		//0.5 degC
-#define TMP100_RESOLUTION_10_BIT			0x20		//0.25 degC
-#define TMP100_RESOLUTION_11_BIT			0x40		//0.125 degC
-#define TMP100_RESOLUTION_12_BIT			0x60		//0.0625 degC
+#define RESOLUTION_9_BIT			0x00		//0.5 degC
+#define RESOLUTION_10_BIT			0x20		//0.25 degC
+#define RESOLUTION_11_BIT			0x40		//0.125 degC
+#define RESOLUTION_12_BIT			0x60		//0.0625 degC
 
-void tmp_init(dev_id id);
-uint8_t tmp_getRawTemperature(const dev_id id,
-                               const int16_t raw,
-                               float *res);
-bool tmp_getTemperature_raw(const dev_id id, int16_t *res);
+//Conversion Constant (in degC*E-4)
+# define	MUL_9_bit				5000
+# define	MUL_10_bit				2500
+# define	MUL_11_bit				1250
+# define	MUL_12_bit				625
+
+class TMP100
+
+{
+protected:	
+	DWire &wire;
+    unsigned char address;
+	unsigned short mul;
+
+public:
+	
+	TMP100(DWire &i2c, unsigned char addr);
+	virtual ~TMP100( ) {};
+	
+	void init(unsigned char res);
+	unsigned char getTemperature(long &t);
+	
+	
+	// read and write from the register
+	unsigned char readRegister(unsigned char reg, unsigned short &output);
+	unsigned char writeRegister(unsigned char reg, unsigned char val);
+	
+private:	
+	
+};
+
+
+
 
 #endif  // TMP100_H
